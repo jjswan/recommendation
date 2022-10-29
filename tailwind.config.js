e78df6bc3b4048f36df3e1e6cc20f7b599985785
jjswan <69625230+jjswan@users.noreply.github.com>
2022-10-29 23:17:04 +0900

    Add files via upload
    Recommendation practice version

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content:[
+    'Recommendation.html'
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      "coralblue": '#99f6e4',
+      // Configure your color palette here
+    },
+    container:{
+      center:true,
+    }
+  }
+}
